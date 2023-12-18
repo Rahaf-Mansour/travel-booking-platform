df6afac0d2f78c5b57ca7d4fd55f05938bc82749
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-18 22:30:52 +0200

    downgrade the date-fns version

--- src/pages/Home/components/DateCheck/DateCheck.jsx
+++ src/pages/Home/components/DateCheck/DateCheck.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+// import PropTypes from "prop-types";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
+function DateCheck() {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(new Date().valueOf() + 1000 * 3600 * 24),
+      key: "selection",
+    },
+  ]);
 
-const DateRange = () => {
-  return <div>Date</div>;
-};
+  return (
+    <div>
+      <DateRange
+        editableDateInputs={true}
+        onChange={(item) => setDate([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={date}
+      />
+    </div>
+  );
+}
 
-export default DateRange;
+// DateRange.prototypes = {
+//   startDate: PropTypes.object,
+//   endDate: PropTypes.object,
+//   color: PropTypes.string,
+//   key: PropTypes.string,
+//   autoFocus: PropTypes.bool,
+//   disabled: PropTypes.bool,
+//   showDateDisplay: PropTypes.bool,
+// };
+
+export default DateCheck;
