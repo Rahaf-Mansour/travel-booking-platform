62b31066a62348a8f8f52d9f216577cb5d6e44bf
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-18 21:49:44 +0200

    add a global searchItem that contains children prop

---
+++ src/pages/Home/components/SearchItem/SearchItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./SearchItem.module.css";
+
+const SearchItem = ({ children }) => {
+  return <div className={styles.heroSearchItem}>{children}</div>;
+};
+
+SearchItem.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default SearchItem;
