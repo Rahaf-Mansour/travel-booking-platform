4feae3505bbcb6b1bc3ed06b259594c66d1800f5
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-19 21:00:23 +0200

    fix the navbar component navigation and styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
+import CustomButton from "../CustomButton";
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
-      <span className={styles.logo}>Booking </span>
-      <button className={styles.navButton}>
-        <NavLink to="/">Logout</NavLink>
-      </button>
+      <CustomButton className={styles.logo}>
+        <NavLink to="/home">Booking</NavLink>
+      </CustomButton>
+      <div>
+        <CustomButton className={styles.navButton}>
+          <NavLink to="/home">Home</NavLink>
+        </CustomButton>
+        <CustomButton className={styles.navButton}>
+          <NavLink to="/search">Search</NavLink>
+        </CustomButton>
+        <CustomButton className={styles.navButton}>
+          <NavLink to="/hotel">Hotel</NavLink>
+        </CustomButton>
+      </div>
     </nav>
   );
 };
