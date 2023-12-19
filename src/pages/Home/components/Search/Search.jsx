90a34cda05e25b1a5651252c5ca8a380fd85ac48
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-19 17:34:05 +0200

    edit the search structure

--- src/pages/Home/components/Search/Search.jsx
+++ src/pages/Home/components/Search/Search.jsx
@@ -2,11 +2,34 @@ import React from "react";
 import styles from "./Search.module.css";
 import SingleBedIcon from "@mui/icons-material/SingleBed";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import DateRangeIcon from "@mui/icons-material/DateRange";
 import SearchItem from "../SearchItem";
 import DateCheck from "../DateCheck";
+import OptionItem from "../OptionItem";
 
 const Search = () => {
+  const initialState = {
+    adults: 2,
+    children: 0,
+    room: 1,
+  };
+
+  const [isOptionsOpened, setIsOptionsOpened] = React.useState(false);
+  const [options, setOptions] = React.useState(initialState);
+
+  const handleIncrement = (option) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      [option]: prevOptions[option] + 1,
+    }));
+  };
+
+  const handleDecrement = (option) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      [option]: prevOptions[option] - 1,
+    }));
+  };
+
   return (
     <div className={styles.hero}>
       <div className={styles.heroSearch}>
@@ -19,14 +42,42 @@ const Search = () => {
           />
         </SearchItem>
         <SearchItem>
-          <DateRangeIcon className={styles.heroIcon} />
           <DateCheck />
         </SearchItem>
         <SearchItem>
-          <PersonOutlineIcon className={styles.heroIcon} />
-          <span className={styles.heroSearchText}>
-            2 adult . 0 children . 1 room
-          </span>
+          <button
+            className={styles.heroSearchText}
+            onClick={() => setIsOptionsOpened(!isOptionsOpened)}
+          >
+            <PersonOutlineIcon className={styles.heroIcon} />
+            <span>
+              {`${options.adults} adult . ${options.children} children . ${options.room} room`}
+            </span>
+          </button>
+          {isOptionsOpened && (
+            <div className={styles.options}>
+              <OptionItem
+                label="Adults"
+                count={options.adults}
+                min={1}
+                onIncrement={() => handleIncrement("adults")}
+                onDecrement={() => handleDecrement("adults")}
+              />
+              <OptionItem
+                label="Children"
+                count={options.children}
+                onIncrement={() => handleIncrement("children")}
+                onDecrement={() => handleDecrement("children")}
+              />
+              <OptionItem
+                label="Rooms"
+                count={options.room}
+                min={1}
+                onIncrement={() => handleIncrement("room")}
+                onDecrement={() => handleDecrement("room")}
+              />
+            </div>
+          )}
         </SearchItem>
         <SearchItem>
           <button className={styles.heroSearchButton}>Search</button>
