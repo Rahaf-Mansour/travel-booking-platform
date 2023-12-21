308f8a21114eb3a5014024a6c448517d1da9b419
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-21 14:57:28 +0200

    use the auth context in the App and permitting a specific routes depending on the UserType

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React from "react";
-// import { useState } from "react";
 import Login from "./pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import Home from "./pages/Home";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import { AuthContext } from "./context/authContext";
 
 function App() {
+  const { user } = React.useContext(AuthContext);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/search" element={<SearchPage />} />
-          {/* <Route path="/adminDashboard" element={<Dashboard />} /> */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {user?.userType == "User" && (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/search" element={<SearchPage />} />
+          </>
+        )}
+        {user?.userType == "Admin" && (
+          <Route path="/adminDashboard" element={<>Hello admin</>} />
+        )}
+
+        {/* <Route path="/adminDashboard" element={<Dashboard />} /> */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
