3c95b3c3881cabe65b96e26999ce7aec4c9f18af
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-21 14:39:02 +0200

    add the same search component in the home page as in the search page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 import HeroSection from "./components/HeroSection";
+import Search from "../SearchPage/components/Search";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <HeroSection />
-      <button className="">Explore Now</button>
-
+      <Search />
     </div>
   );
 };
