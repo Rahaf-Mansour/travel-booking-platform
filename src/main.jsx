8f26196aa2d37bcd1f66cd9e11b51d0e89559935
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-22 22:36:12 +0200

    wrap the app with a search context provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import AuthContextProvider from "./context/authContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import SearchContextProvider from "./context/searchContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthContextProvider>
-      <App />
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
     </AuthContextProvider>
   </BrowserRouter>
 );
