aa40aeb6e22cd73ecb5f8ba47b115d4fafe14227
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-02 15:45:32 +0200

    edit the search context to be accept the loader context and define a new state for the searchProps

--- src/context/searchContext.jsx
+++ src/context/searchContext.jsx
@@ -1,17 +1,44 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types";
+import { searchAPI } from "../services/searchService";
+import dayjs from "dayjs";
+import { LoaderContext } from "./LoaderContext";
 
-const SearchContext = createContext();
+export const SearchContext = createContext();
 
 export const SearchContextProvider = ({ children }) => {
-  const [searchData, setSearchData] = useState({});
+  const [searchData, setSearchData] = useState([]);
+  const [searchProps, setSearchProps] = useState({
+    city: "",
+    checkInDate: dayjs().format("YYYY-MM-DD"),
+    checkOutDate: dayjs().add(1, "day").format("YYYY-MM-DD"),
+    adults: 2,
+    children: 0,
+    room: 1,
+  });
+  const { startLoader, stopLoader } = useContext(LoaderContext);
 
-  const updateSearchData = (newData) => {
-    setSearchData(newData);
+  const callSearchAPI = async (values) => {
+    try {
+      startLoader();
+      const response = await searchAPI(values);
+      setSearchProps((prevProps) => ({ ...prevProps, ...values }));
+      setSearchData((prevData) => [...prevData, ...response]);
+    } catch (error) {
+      console.log(error.message || "Error: Can't search");
+    } finally {
+      stopLoader();
+    }
   };
 
   return (
-    <SearchContext.Provider value={{ searchData, updateSearchData }}>
+    <SearchContext.Provider
+      value={{
+        searchData,
+        callSearchAPI,
+        searchProps,
+      }}
+    >
       {children}
     </SearchContext.Provider>
   );
