ac05d2f40662e240c318c68fc0d96436830cfdfe
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-02 18:51:57 +0200

    edit the search result component by removing filters from it

--- src/pages/SearchPage/components/SearchResult/SearchResult.jsx
+++ src/pages/SearchPage/components/SearchResult/SearchResult.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import styles from "./style.module.css";
 import SearchResultItem from "../SearchResultItem/SearchResultItem";
-import SearchFilters from "../SearchFilters";
 import { SearchContext } from "../../../../context/searchContext";
 import { Link } from "react-router-dom";
 
@@ -14,21 +13,16 @@ const SearchResult = () => {
 
   const hotelData = searchData;
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <SearchFilters />
-        <div className={styles.resultList}>
-          {hotelData.map((hotel) => (
-            <Link
-              to={`/hotel/${hotel.hotelId}`}
-              key={hotel.hotelId}
-              style={{ textDecoration: "none" }}
-            >
-              <SearchResultItem key={hotel.hotelId} hotel={hotel} />
-            </Link>
-          ))}
-        </div>
-      </div>
+    <div className={styles.resultList}>
+      {hotelData.map((hotel) => (
+        <Link
+          to={`/hotel/${hotel.hotelId}`}
+          key={hotel.hotelId}
+          style={{ textDecoration: "none" }}
+        >
+          <SearchResultItem key={hotel.hotelId} hotel={hotel} />
+        </Link>
+      ))}
     </div>
   );
 };
