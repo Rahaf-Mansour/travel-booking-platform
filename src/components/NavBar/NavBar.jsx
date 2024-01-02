5b8425172389223fd0c5b0f29ebc456d340f51b0
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-02 22:34:15 +0200

    edit the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,133 +1,315 @@
-import * as React from "react";
-import PropTypes from "prop-types";
+// import * as React from "react";
+// import PropTypes from "prop-types";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Divider from "@mui/material/Divider";
+// import Drawer from "@mui/material/Drawer";
+// import IconButton from "@mui/material/IconButton";
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemText from "@mui/material/ListItemText";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import Button from "@mui/material/Button";
+// import { useNavigate } from "react-router-dom";
+// import colors from "../../constants/colorConstants";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import { Link } from "react-router-dom";
+
+// const drawerWidth = 240;
+// const navItems = ["Home", "Search"];
+
+// function NavBar(props) {
+//   const navigate = useNavigate();
+//   const { window } = props;
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+//   const handleDrawerToggle = () => {
+//     setMobileOpen((prevState) => !prevState);
+//   };
+
+//   const handleNavigate = (event, item) => {
+//     event.preventDefault();
+//     navigate(`/${item.toLowerCase()}`);
+//   };
+
+//   const drawer = (
+//     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+//       <Typography variant="h6" sx={{ my: 2 }}>
+//         Booking
+//       </Typography>
+//       <Divider />
+//       <List>
+//         {navItems.map((item) => (
+//           <ListItem key={item} disablePadding>
+//             <ListItemButton
+//               sx={{ textAlign: "center" }}
+//               onClick={() => handleNavigate(event, item)}
+//             >
+//               <ListItemText primary={item} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </Box>
+//   );
+
+//   const container =
+//     window !== undefined ? () => window().document.body : undefined;
+
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       <CssBaseline />
+//       <AppBar
+//         component="nav"
+//         sx={{
+//           backgroundColor: colors.primaryColor,
+//           paddingX: { md: "3rem", sm: "none" },
+//         }}
+//       >
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: "none" } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography
+//             variant="h6"
+//             component="div"
+//             sx={{
+//               flexGrow: 1,
+//               display: { xs: "none", sm: "block" },
+//               fontSize: "1.5rem",
+//             }}
+//           >
+//             Booking
+//           </Typography>
+//           <Box sx={{ display: { xs: "none", sm: "block" } }}>
+//             {navItems.map((item) => (
+//               <Button
+//                 onClick={() => handleNavigate(event, item)}
+//                 key={item}
+//                 sx={{ color: "#fff", fontSize: "1rem", marginLeft: 2 }}
+//               >
+//                 {item}
+//               </Button>
+//             ))}
+//             <Button component={Link} to="/myCart" color="inherit">
+//               <ShoppingCartIcon />
+//             </Button>
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//       <nav>
+//         <Drawer
+//           container={container}
+//           variant="temporary"
+//           open={mobileOpen}
+//           onClose={handleDrawerToggle}
+//           ModalProps={{
+//             keepMounted: true,
+//           }}
+//           sx={{
+//             display: { xs: "block", sm: "none" },
+//             "& .MuiDrawer-paper": {
+//               boxSizing: "border-box",
+//               width: drawerWidth,
+//             },
+//           }}
+//         >
+//           {drawer}
+//         </Drawer>
+//       </nav>
+//     </Box>
+//   );
+// }
+
+// NavBar.propTypes = {
+//   window: PropTypes.func,
+// };
+
+// export default NavBar;
+
+import React from "react";
+import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import colors from "../../constants/colorConstants";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
-import colors from "../../constants/colorConstants";
+import { useTheme } from "@mui/material/styles";
 
-const drawerWidth = 240;
-const navItems = ["Home", "Search", "Hotel"];
+const NavBar = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme?.breakpoints?.down("sm"));
 
-function NavBar(props) {
-  const navigate = useNavigate();
-  const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+    setMobileOpen(!mobileOpen);
   };
+
+  const navItems = ["Home", "Search", "Cart"];
+  const navigate = useNavigate();
 
   const handleNavigate = (event, item) => {
     event.preventDefault();
     navigate(`/${item.toLowerCase()}`);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        Booking
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton
-              sx={{ textAlign: "center" }}
-              onClick={() => handleNavigate(event, item)}
-            >
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
-  return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        component="nav"
+  const renderAppBar = () => (
+    <AppBar
+      position="static"
+      component="nav"
+      sx={{ backgroundColor: colors.primaryColor }}
+    >
+      <Toolbar
         sx={{
-          backgroundColor: colors.primaryColor,
-          paddingX: { md: "3rem", sm: "none" },
+          display: "flex",
+          justifyContent: "space-between",
+          paddingX: { md: "7rem", sm: "2rem" },
         }}
       >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", sm: "block" },
-              fontSize: "1.5rem",
-            }}
-          >
-            Booking
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button
-                onClick={() => handleNavigate(event, item)}
-                key={item}
-                sx={{ color: "#fff", fontSize: "1rem", marginLeft: 2 }}
-              >
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          color="inherit"
           sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
+            textDecoration: "none",
+            fontSize: "1.5rem",
+            display: { xs: "none", md: "block" },
           }}
         >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
-  );
-}
+          Booking
+        </Typography>
 
-NavBar.propTypes = {
-  window: PropTypes.func,
+        <div>
+          <Button
+            component={Link}
+            to="/home"
+            color="inherit"
+            sx={{ marginRight: 2 }}
+          >
+            <HomeIcon />
+            <Typography
+              variant="body1"
+              sx={{ marginLeft: 1, display: { xs: "none", md: "block" } }}
+            >
+              Home
+            </Typography>
+          </Button>
+
+          <Button
+            component={Link}
+            to="/search"
+            color="inherit"
+            sx={{ marginRight: 2 }}
+          >
+            <SearchIcon />
+            <Typography
+              variant="body1"
+              sx={{ marginLeft: 1, display: { xs: "none", md: "block" } }}
+            >
+              Search
+            </Typography>
+          </Button>
+
+          <Button component={Link} to="/myCart" color="inherit">
+            <ShoppingCartIcon />
+            <Typography
+              variant="body1"
+              sx={{ marginLeft: 1, display: { xs: "none", md: "block" } }}
+            >
+              My Cart
+            </Typography>
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+
+  const renderDrawer = () => (
+    <Drawer
+      anchor="left"
+      open={mobileOpen}
+      onClose={handleDrawerToggle}
+      sx={{ "& .MuiDrawer-paper": { width: "240px" } }}
+    >
+      <Box>
+        <Typography variant="h6" sx={{ my: 2, textAlign: "center" }}>
+          Booking
+        </Typography>
+        <Divider />
+        <List>
+          {navItems.map((item) => (
+            <ListItem key={item} disablePadding>
+              <ListItemButton
+                sx={{ textAlign: "center" }}
+                onClick={() => handleNavigate(event, item)}
+              >
+                <ListItemText primary={item} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+
+  return (
+    <>
+      {isSmallScreen ? (
+        <>
+          {renderDrawer()}
+          <AppBar
+            position="static"
+            sx={{
+              backgroundColor: colors.primaryColor,
+
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{
+                marginRight: "auto",
+              }}
+            >
+              <MenuIcon sx={{ marginLeft: "2rem" }} />
+            </IconButton>
+          </AppBar>
+        </>
+      ) : (
+        renderAppBar()
+      )}
+    </>
+  );
 };
 
 export default NavBar;
