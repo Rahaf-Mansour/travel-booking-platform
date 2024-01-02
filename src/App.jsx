fc79c084581f50969610cefb7f74edf6d778b848
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-02 19:53:01 +0200

    edit the app structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,35 @@ import Login from "./pages/Login";
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import Home from "./pages/Home";
-import SearchPage from "./pages/SearchPage/SearchPage";
+import SearchPage from "./pages/SearchPage/SearchPage.jsx";
 import { AuthContext } from "./context/authContext";
+import SearchContextProvider from "./context/searchContext.jsx";
+import Hotel from "./pages/Hotel";
 
 function App() {
   const { user } = React.useContext(AuthContext);
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {user?.userType == "User" && (
-          <>
-            <Route path="/home" element={<Home />} />
-            <Route path="/search" element={<SearchPage />} />
-          </>
-        )}
-        {user?.userType == "Admin" && (
-          <Route path="/adminDashboard" element={<>Hello admin</>} />
-        )}
+      <SearchContextProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {user?.userType == "User" && (
+            <>
+              <Route path="/home" element={<Home />} />
+              <Route path="/search" element={<SearchPage />} />
+              {/* <Route path="/hotel" element={<Hotel />} /> */}
+              <Route path="/hotel/:hotelId" element={<Hotel />} />
+            </>
+          )}
+          {user?.userType == "Admin" && (
+            <Route path="/adminDashboard" element={<>Hello admin</>} />
+          )}
 
-        {/* <Route path="/adminDashboard" element={<Dashboard />} /> */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          {/* <Route path="/adminDashboard" element={<Dashboard />} /> */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </SearchContextProvider>
     </>
   );
 }
