bd3989b7a006f256eef72cf843c9a171f68f215a
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-08 20:59:02 +0200

    edit the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 import HeroSection from "./components/HeroSection";
-import Search from "../SearchPage/components/Search";
-// import FeaturedDeals from "./components/FeaturedDeals";
+import SearchBar from "../SearchPage/components/SearchBar";
+import FeaturedDeals from "./components/FeaturedDeals";
 import styles from "./style.module.css";
-import Deals from "./components/FeaturedDeals/Deals";
 import TrendingDestinations from "./components/TrendingDestinations";
 import RecentlyVisitedHotels from "./components/RecentlyVisitedHotels";
 
@@ -13,11 +12,9 @@ const Home = () => {
     <>
       <NavBar />
       <HeroSection />
-      <Search topXs="30px" topLg="-33px" />
+      <SearchBar topXs="30px" topLg="-33px" />
       <div className={styles.container}>
-        {/* <FeaturedDeals />
-        <FeaturedDeals /> */}
-        <Deals />
+        <FeaturedDeals />
         <TrendingDestinations />
         <RecentlyVisitedHotels />
       </div>
