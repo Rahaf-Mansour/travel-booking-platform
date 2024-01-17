5d8bf0eff59d308db88a30fcf5dec75040a756fb
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-17 21:32:01 +0200

    edit the authContext file

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,18 +1,32 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import axiosInstance from "../Axios/axiosInstance";
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [cookies, setCookie, removeCookie] = useCookies(["auth"]);
-  const [user, setUser] = React.useState(null);
+  const [user, setUser] = useState(cookies["auth"]);
   const navigate = useNavigate();
 
   const loginUser = (authData) => {
     setCookie("auth", authData);
+    setUser(authData);
   };
+
+  useEffect(() => {
+    setUser(cookies["authData"]);
+  }, [cookies]);
+
+  if (user) {
+    axiosInstance.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${user.authentication}`;
+  } else {
+    delete axiosInstance.defaults.headers.common["Authorization"];
+  }
 
   const logoutUser = () => {
     setUser(null);
