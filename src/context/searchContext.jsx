cc6f226185157ff5681d5190e0e2b1f6d94f5cd8
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-18 22:50:46 +0200

    edit search context structure

--- src/context/searchContext.jsx
+++ src/context/searchContext.jsx
@@ -1,44 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import PropTypes from "prop-types";
-import { searchAPI } from "../services/searchService";
-import dayjs from "dayjs";
-import { LoaderContext } from "./LoaderContext";
 
 export const SearchContext = createContext();
 
 export const SearchContextProvider = ({ children }) => {
-  const [searchData, setSearchData] = useState([]);
-  const [searchProps, setSearchProps] = useState({
-    city: "",
-    checkInDate: dayjs().format("YYYY-MM-DD"),
-    checkOutDate: dayjs().add(1, "day").format("YYYY-MM-DD"),
-    adults: 2,
-    children: 0,
-    room: 1,
+  const [searchParams, setSearchParams] = useState({
+    checkInDate: null,
+    checkOutDate: null,
   });
-  const { startLoader, stopLoader } = useContext(LoaderContext);
 
-  const callSearchAPI = async (values) => {
-    try {
-      startLoader();
-      const response = await searchAPI(values);
-      setSearchProps((prevProps) => ({ ...prevProps, ...values }));
-      setSearchData((prevData) => [...prevData, ...response]);
-    } catch (error) {
-      console.log(error.message || "Error: Can't search");
-    } finally {
-      stopLoader();
-    }
+  const updateSearchParams = (newParams) => {
+    setSearchParams({ ...searchParams, ...newParams });
   };
 
   return (
-    <SearchContext.Provider
-      value={{
-        searchData,
-        callSearchAPI,
-        searchProps,
-      }}
-    >
+    <SearchContext.Provider value={{ searchParams, updateSearchParams }}>
       {children}
     </SearchContext.Provider>
   );
