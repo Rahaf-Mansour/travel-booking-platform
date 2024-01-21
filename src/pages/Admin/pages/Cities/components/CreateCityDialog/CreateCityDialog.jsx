8ce18e9e1afbe71230e7a505fc8a14565027651b
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-22 01:27:00 +0200

    add the logic for creating a city

---
+++ src/pages/Admin/pages/Cities/components/CreateCityDialog/CreateCityDialog.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Dialog, DialogTitle } from "@mui/material";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import PropTypes from "prop-types";
+import { postNewCity } from "../../../../../../services/manageCities";
+import CreateEntityDialog from "../../../../components/CreateEntityDialog";
+import CreateButton from "../../../../components/CreateButton/CreateButton";
+
+const CreateCityDialog = ({ addCity, snackbarProps }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  const fields = [
+    { name: "name", label: "Name", type: "text" },
+    { name: "description", label: "Description", type: "text" },
+  ];
+
+  const initialValues = fields.reduce((values, field) => {
+    values[field.name] = "";
+    return values;
+  }, {});
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    description: Yup.string().required("Description is required"),
+  });
+
+  const handleCreateEntity = async (values, actions) => {
+    try {
+      const newEntity = await postNewCity(values.name, values.description);
+      console.log("City created:", newEntity);
+      snackbarProps.showSuccessSnackbar("New city created successfully!");
+      addCity(newEntity);
+    } catch (error) {
+      snackbarProps.showErrorSnackbar(`Whoops! ${error.message}`);
+    } finally {
+      handleDialogClose();
+      actions.setSubmitting(false);
+    }
+  };
+
+  return (
+    <div>
+      <CreateButton handleDialogOpen={handleDialogOpen} />
+
+      <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>Create New City</DialogTitle>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleCreateEntity}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <CreateEntityDialog
+              fields={fields}
+              touched={touched}
+              errors={errors}
+              handleDialogClose={handleDialogClose}
+              isSubmitting={isSubmitting}
+            />
+          )}
+        </Formik>
+      </Dialog>
+    </div>
+  );
+};
+
+export default CreateCityDialog;
+
+CreateCityDialog.propTypes = {
+  addCity: PropTypes.func.isRequired,
+  snackbarProps: PropTypes.shape({
+    open: PropTypes.bool,
+    message: PropTypes.string,
+    severity: PropTypes.string,
+    handleCloseSnackbar: PropTypes.func,
+    showSuccessSnackbar: PropTypes.func,
+    showErrorSnackbar: PropTypes.func,
+  }),
+};
