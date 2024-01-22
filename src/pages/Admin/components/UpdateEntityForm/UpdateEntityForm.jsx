33298b3cac661ac54cf7df8faab88249ed675827
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-22 19:36:38 +0200

    create a shared update fields

--- src/pages/Admin/components/UpdateEntityForm/UpdateEntityForm.jsx
+++ src/pages/Admin/components/UpdateEntityForm/UpdateEntityForm.jsx
@@ -1,126 +1,41 @@
 import React from "react";
-import {
-  Drawer,
-  IconButton,
-  Typography,
-  Box,
-  TextField,
-  Button,
-} from "@mui/material";
-import { ChevronRight as ChevronRightIcon } from "@mui/icons-material";
-import { Formik, Form, Field } from "formik";
-import * as Yup from "yup";
+import { Field } from "formik";
+import { TextField } from "@mui/material";
 import PropTypes from "prop-types";
-import { updateCity } from "../../../../services/manageCities";
-// import GenericSnackbar from "../../../../components/GenericSnackbar/GenericSnackbar";
-// import useSnackbar from "../../../../hooks/useSnackbar";
 
-const UpdateEntityForm = ({ open, onClose, entityData, onUpdate }) => {
-  if (!entityData) {
-    return null;
-  }
-
-  const validationSchema = Yup.object({
-    name: Yup.string().required("Name is required"),
-    description: Yup.string().required("Description is required"),
-  });
-
-  const initialValues = {
-    name: entityData.name || "",
-    description: entityData.description || "",
-  };
-
-  const handleUpdateClick = async (values, actions) => {
-    try {
-      await updateCity(entityData.id, values.name, values.description);
-
-      const updatedEntity = {
-        ...entityData,
-        name: values.name,
-        description: values.description,
-      };
-
-      if (onUpdate) {
-        onUpdate(updatedEntity);
-      }
-      console.log("Entity updated:", updatedEntity);
-    } catch (error) {
-      console.log(error.message);
-    } finally {
-      onClose();
-      actions.setSubmitting(false);
-    }
-  };
-
+const UpdateEntityForm = ({ fields, touched, errors }) => {
   return (
-    <Drawer anchor="right" open={open} onClose={onClose} variant="persistent">
-      <Box
-        sx={{ width: 250 }}
-        role="presentation"
-        display="flex"
-        flexDirection="column"
-      >
-        <IconButton onClick={onClose}>
-          <ChevronRightIcon />
-        </IconButton>
-        <Typography variant="h6" sx={{ ml: 2 }}>
-          Update Entity
-        </Typography>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleUpdateClick}
-        >
-          {({ errors, touched, isSubmitting }) => (
-            <Form>
-              <Box sx={{ m: 2 }}>
-                <Field
-                  name="name"
-                  label="Name"
-                  variant="standard"
-                  fullWidth
-                  as={TextField}
-                  error={touched.name && Boolean(errors.name)}
-                  helperText={touched.name && errors.name}
-                />
-                <Field
-                  name="description"
-                  label="Description"
-                  variant="standard"
-                  fullWidth
-                  multiline
-                  as={TextField}
-                  error={touched.description && Boolean(errors.description)}
-                  helperText={touched.description && errors.description}
-                />
-                <Box mt={2}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    Update
-                  </Button>
-                </Box>
-              </Box>
-            </Form>
-          )}
-        </Formik>
-      </Box>
-    </Drawer>
+    <>
+      {fields.map((field) => (
+        <Field
+          sx={{ marginBottom: 2 }}
+          key={field.name}
+          as={TextField}
+          name={field.name}
+          label={field.label}
+          fullWidth
+          variant="standard"
+          type={field.type}
+          error={touched[field.name] && Boolean(errors[field.name])}
+          helperText={touched[field.name] && errors[field.name]}
+        />
+      ))}
+    </>
   );
 };
 
-UpdateEntityForm.propTypes = {
-  open: PropTypes.bool,
-  onClose: PropTypes.func,
-  entityData: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string,
-    description: PropTypes.string,
-  }),
-  onUpdate: PropTypes.func,
-};
-
 export default UpdateEntityForm;
+
+UpdateEntityForm.propTypes = {
+  fields: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      type: PropTypes.string,
+    })
+  ),
+  touched: PropTypes.object,
+  errors: PropTypes.object,
+  handleChange: PropTypes.func,
+  values: PropTypes.object,
+};
