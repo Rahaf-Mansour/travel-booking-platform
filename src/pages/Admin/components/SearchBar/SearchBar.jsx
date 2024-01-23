6c8edfa1bfcca381c8aa0674dda5a936eae3025f
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-24 01:04:26 +0200

    create a shared search bar component between cities and hotels

--- src/pages/Admin/components/SearchBar/SearchBar.jsx
+++ src/pages/Admin/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,31 @@
-import React from "react";
-import { Paper, InputBase, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Paper,
+  InputBase,
+  IconButton,
+  FormControl,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import PropTypes from "prop-types";
+import styles from "./style.module.css";
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchType, setSearchType] = useState("name");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm, searchType);
+  };
+
   return (
     <Paper
       component="form"
+      onSubmit={handleSearch}
       sx={{
-        p: "2px 4px",
+        p: "1px 4px",
         display: "flex",
         alignItems: "center",
         width: "50%",
@@ -15,11 +33,25 @@ const SearchBar = () => {
         borderRadius: "5px",
         mr: 1,
       }}
+      className={styles.searchBar}
     >
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 110 }}>
+        <Select
+          value={searchType}
+          onChange={(e) => setSearchType(e.target.value)}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem value="name">Name</MenuItem>
+          <MenuItem value="description">Description</MenuItem>
+        </Select>
+      </FormControl>
       <InputBase
         sx={{ ml: 1, flex: 1 }}
-        placeholder="Search by string"
-        inputProps={{ "aria-label": "search by string" }}
+        placeholder={`Search by ${searchType}`}
+        inputProps={{ "aria-label": `search by ${searchType}` }}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
         <SearchIcon />
@@ -29,3 +61,7 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
+
+SearchBar.propTypes = {
+  onSearch: PropTypes.func,
+};
