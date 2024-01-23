b705cdaee9a0c355126389828e7d21abbc2487d1
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-24 01:41:05 +0200

    edit admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import Login from "./pages/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom"; // 'Navigate' is used instead of 'Redirect', and 'Switch' is removed
 import PageNotFound from "./pages/PageNotFound";
 import Home from "./pages/Home";
-import SearchPage from "./pages/SearchPage/SearchPage.jsx";
+import SearchPage from "./pages/SearchPage";
 import { AuthContext } from "./context/authContext";
-import SearchContextProvider from "./context/searchContext.jsx";
 import Hotel from "./pages/Hotel";
+import Checkout from "./pages/Checkout";
+import Confirmation from "./pages/Confirmation";
+import Cities from "./pages/Admin/pages/Cities";
+import Hotels from "./pages/Admin/pages/Hotels";
+import Rooms from "./pages/Admin/pages/Rooms/Rooms";
 
 function App() {
   const { user } = React.useContext(AuthContext);
 
   return (
     <>
-      <SearchContextProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          {user?.userType == "User" && (
-            <>
-              <Route path="/home" element={<Home />} />
-              <Route path="/search" element={<SearchPage />} />
-              {/* <Route path="/hotel" element={<Hotel />} /> */}
-              <Route path="/hotel/:hotelId" element={<Hotel />} />
-            </>
-          )}
-          {user?.userType == "Admin" && (
-            <Route path="/adminDashboard" element={<>Hello admin</>} />
-          )}
-
-          {/* <Route path="/adminDashboard" element={<Dashboard />} /> */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </SearchContextProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {user?.userType === "User" && (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/hotel/:hotelId" element={<Hotel />} />
+            <Route path="/cart" element={<Checkout />} />
+            <Route path="/confirmation" element={<Confirmation />} />
+          </>
+        )}
+        {user?.userType === "Admin" && (
+          <>
+            <Route path="/adminDashboard/cities" element={<Cities />} />
+            <Route path="/adminDashboard/hotels" element={<Hotels />} />
+            <Route path="/adminDashboard/rooms" element={<Rooms />} />
+            <Route
+              path="/adminDashboard/*"
+              element={<Navigate to="/adminDashboard/cities" replace />}
+            />
+          </>
+        )}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
