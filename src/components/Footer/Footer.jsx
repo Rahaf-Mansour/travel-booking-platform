a2d7ac353a16ac44b8b6fe4183c76601173a4ee5
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-24 02:43:08 +0200

    edit the footer code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,80 +5,36 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
-    // <Box
-    //   sx={{
-    //     bgcolor: "#395591",
-    //     color: "white",
-    //     paddingTop: 3,
-    //     paddingBottom: 1,
-    //     marginTop: "2rem",
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     alignItems: "center",
-    //     justifyContent: "center",
-    //   }}
-    // >
-    //   <Typography variant="body1" component="p">
-    //     Developed by Rahaf Mansour
-    //   </Typography>
-    //   <Typography variant="body1" component="p">
-    //     All rights reserved © {new Date().getFullYear()}
-    //   </Typography>
-    //   <Box
-    //     sx={{
-    //       m: 0.5,
-    //     }}
-    //   >
-    //     <IconButton
-    //       aria-label="GitHub"
-    //       component={Link}
-    //       href="https://github.com/Rahaf-Mansour"
-    //       target="_blank"
-    //       color="inherit"
-    //     >
-    //       <GitHubIcon />
-    //     </IconButton>
-    //     <IconButton
-    //       aria-label="LinkedIn"
-    //       component={Link}
-    //       href="https://www.linkedin.com/in/rahafmansour/"
-    //       target="_blank"
-    //       color="inherit"
-    //     >
-    //       <LinkedInIcon />
-    //     </IconButton>
-    //   </Box>
-    // </Box>
     <Box
-      display="flex"
-      flexDirection="column"
-      minHeight="20vh" // Makes the main container at least as tall as the viewport
+      sx={{
+        bgcolor: "#395591",
+        color: "white",
+        paddingTop: 3,
+        paddingBottom: 1,
+        marginTop: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
     >
+      <Typography variant="body1" component="p">
+        Developed by Rahaf Mansour
+      </Typography>
+      <Typography variant="body1" component="p">
+        All rights reserved © {new Date().getFullYear()}
+      </Typography>
       <Box
-        component="main"
-        // flexGrow={1} // Allows this box to grow and take up any available space
-      ></Box>
-
-      <Box
-        component="footer"
         sx={{
-          paddingTop: 2,
-          paddingBottom: 1,
-          mt: "auto", // Pushes the footer to the bottom
-          backgroundColor: "#395591",
-          color: "white",
-          textAlign: "center",
+          m: 0.5,
         }}
       >
-        <Typography variant="body1">Developed by Rahaf Mansour</Typography>
-        <Typography variant="body1">All rights reserved © 2024</Typography>
         <IconButton
           aria-label="GitHub"
           component={Link}
           href="https://github.com/Rahaf-Mansour"
           target="_blank"
           color="inherit"
-          sx={{ marginRight: 1 }}
         >
           <GitHubIcon />
         </IconButton>
