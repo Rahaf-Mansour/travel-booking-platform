4d136fac0a7e688d39e20c196c42c3a26bd44915
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-25 01:57:01 +0200

    refactor the code with the custom data fetching

--- src/pages/Hotel/Hotel.jsx
+++ src/pages/Hotel/Hotel.jsx
@@ -1,99 +1,83 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import NavBar from "../../components/NavBar";
-import styles from "./style.module.css";
+import Footer from "../../components/Footer";
+import CircularProgressIndicator from "../../components/CircularProgressIndicator";
+import { useHotelData } from "./hooks/useHotelData";
 import HotelDetails from "./components/HotelDetails";
 import HotelMapLocation from "./components/HotelMapLocation";
 import AvailableRooms from "./components/AvailableRooms";
 import VisualGallery from "./components/VisualGallery";
+import GenericSnackbar from "../../components/GenericSnackbar";
+import useSnackbar from "../../hooks/useSnackbar";
 import { useParams } from "react-router-dom";
 import { SearchContext } from "../../context/searchContext";
-
-import {
-  getHotelDetails,
-  getHotelPicturesGallery,
-  getHotelAvailableRooms,
-  getHotelGuestReviews,
-} from "../../services/hotelPageServices";
-import Footer from "../../components/Footer";
+import styles from "./style.module.css";
 
 const Hotel = () => {
   const { hotelId } = useParams();
-  const [hotelDetails, setHotelDetails] = useState(null);
-  const [hotelGuestReviews, setHotelGuestReviews] = useState([]);
-  const [hotelGallery, setHotelGallery] = useState([]);
-  const [hotelAvailableRooms, setHotelAvailableRooms] = useState([]);
   const { searchParams } = useContext(SearchContext);
   const { checkInDate, checkOutDate } = searchParams;
   const isThereDates = checkInDate !== null && checkOutDate !== null;
 
+  const {
+    hotelDetails,
+    hotelGuestReviews,
+    hotelGallery,
+    hotelAvailableRooms,
+    isLoading,
+    error,
+    fetchHotelData,
+  } = useHotelData(hotelId, isThereDates, checkInDate, checkOutDate);
+
+  const { snackbar, showErrorSnackbar, handleCloseSnackbar } = useSnackbar();
+
   useEffect(() => {
-    getHotelDetails(hotelId)
-      .then((details) => {
-        setHotelDetails(details);
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
+    fetchHotelData();
+  }, [hotelId, checkInDate, checkOutDate, fetchHotelData]);
 
-    getHotelGuestReviews(hotelId)
-      .then((reviews) => {
-        setHotelGuestReviews(reviews);
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-
-    getHotelPicturesGallery(hotelId)
-      .then((gallery) => {
-        setHotelGallery(gallery);
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-
-    if (isThereDates) {
-      getHotelAvailableRooms(hotelId, checkInDate, checkOutDate)
-        .then((rooms) => {
-          setHotelAvailableRooms(rooms);
-        })
-        .catch((error) => {
-          console.error(error.message);
-        });
+  useEffect(() => {
+    if (error) {
+      showErrorSnackbar(error);
     }
-  }, [hotelId, checkInDate, checkOutDate]);
+  }, [error]);
 
   return (
     <>
       <NavBar />
-      <div>
-        <div className={styles.container}>
-          <div className={styles.hotelDetailsAndMapContainer}>
-            <HotelDetails
-              hotelDetails={hotelDetails}
-              hotelGuestReviews={hotelGuestReviews}
-            />
-            {hotelDetails &&
-              hotelDetails.latitude &&
-              hotelDetails.longitude && (
-                <HotelMapLocation
-                  latitude={hotelDetails.latitude}
-                  longitude={hotelDetails.longitude}
-                  hotelName={hotelDetails.hotelName}
-                  location={hotelDetails.location}
-                />
-              )}
-          </div>
+      {isLoading ? (
+        <CircularProgressIndicator />
+      ) : (
+        <div>
+          <div className={styles.container}>
+            <div className={styles.hotelDetailsAndMapContainer}>
+              <HotelDetails
+                hotelDetails={hotelDetails}
+                hotelGuestReviews={hotelGuestReviews}
+              />
+              {hotelDetails &&
+                hotelDetails.latitude &&
+                hotelDetails.longitude && (
+                  <HotelMapLocation
+                    latitude={hotelDetails.latitude}
+                    longitude={hotelDetails.longitude}
+                    hotelName={hotelDetails.hotelName}
+                    location={hotelDetails.location}
+                  />
+                )}
+            </div>
 
-          <div className={styles.galleryAndRoomsContainer}>
-            <VisualGallery hotelGallery={hotelGallery} />
-            <AvailableRooms
-              hotelAvailableRooms={hotelAvailableRooms}
-              isThereDates={isThereDates}
-            />
+            <div className={styles.galleryAndRoomsContainer}>
+              <VisualGallery hotelGallery={hotelGallery} />
+              <AvailableRooms
+                hotelAvailableRooms={hotelAvailableRooms}
+                isThereDates={isThereDates}
+              />
+            </div>
           </div>
+          <Footer />
         </div>
-      </div>
-      <Footer />
+      )}
+      <GenericSnackbar {...snackbar} onClose={handleCloseSnackbar} />
     </>
   );
 };
