8dc88c2c403191c336fddcfe86b92e66e312d5cf
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-25 16:02:40 +0200

    enable reinitialization in formik

--- src/pages/Admin/pages/Rooms/components/UpdateRoomForm/UpdateRoomForm.jsx
+++ src/pages/Admin/pages/Rooms/components/UpdateRoomForm/UpdateRoomForm.jsx
@@ -63,6 +63,7 @@ const UpdateRoomForm = ({ open, onClose, entityData, onUpdate }) => {
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={handleUpdateClick}
+          enableReinitialize={true}
         >
           {({ errors, touched, isSubmitting }) => (
             <Form>
