cd0f2369e485621d2012eaa1bad8b16b0d95ccd5
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-01-29 03:25:55 +0200

    rewrite the routes code using protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,40 @@
 import React from "react";
 import Login from "./pages/Login";
-import { Routes, Route, Navigate } from "react-router-dom"; // 'Navigate' is used instead of 'Redirect', and 'Switch' is removed
+import { Routes, Route, Navigate } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import Home from "./pages/Home";
 import SearchPage from "./pages/SearchPage";
-import { AuthContext } from "./context/authContext";
 import Hotel from "./pages/Hotel";
 import Checkout from "./pages/Checkout";
 import Confirmation from "./pages/Confirmation";
 import Cities from "./pages/Admin/pages/Cities";
 import Hotels from "./pages/Admin/pages/Hotels";
 import Rooms from "./pages/Admin/pages/Rooms/Rooms";
+import { withAdminProtection } from "./components/ProtectedRoutes/ProtectedRoutes";
+
+const ProtectedCities = withAdminProtection(Cities);
+const ProtectedHotels = withAdminProtection(Hotels);
+const ProtectedRooms = withAdminProtection(Rooms);
 
 function App() {
-  const { user } = React.useContext(AuthContext);
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {user?.userType === "User" && (
-          <>
-            <Route path="/home" element={<Home />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/hotel/:hotelId" element={<Hotel />} />
-            <Route path="/cart" element={<Checkout />} />
-            <Route path="/confirmation" element={<Confirmation />} />
-          </>
-        )}
-        {user?.userType === "Admin" && (
-          <>
-            <Route path="/adminDashboard/cities" element={<Cities />} />
-            <Route path="/adminDashboard/hotels" element={<Hotels />} />
-            <Route path="/adminDashboard/rooms" element={<Rooms />} />
-            <Route
-              path="/adminDashboard/*"
-              element={<Navigate to="/adminDashboard/cities" replace />}
-            />
-          </>
-        )}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/hotel/:hotelId" element={<Hotel />} />
+      <Route path="/cart" element={<Checkout />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+      <Route
+        path="/adminDashboard"
+        element={<Navigate to="/adminDashboard/cities" replace />}
+      />
+      <Route path="/adminDashboard/cities" element={<ProtectedCities />} />
+      <Route path="/adminDashboard/hotels" element={<ProtectedHotels />} />
+      <Route path="/adminDashboard/rooms" element={<ProtectedRooms />} />
+
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
