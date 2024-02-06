7eb19f458b29a75014469cf6512cd677bb0a63e5
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-02-06 22:16:32 +0200

    put the base url in the .env file

--- src/Axios/axiosInstance.js
+++ src/Axios/axiosInstance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  "https://app-hotel-reservation-webapi-uae-dev-001.azurewebsites.net/api";
-
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default axiosInstance;
