d57da4cb08191435b2457764f5e5b46185c20ac6
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-02-12 22:10:27 +0200

    remove the inline sx styling from paper and assign it to the style file

--- src/pages/Admin/components/SearchBar/SearchBar.jsx
+++ src/pages/Admin/components/SearchBar/SearchBar.jsx
@@ -24,34 +24,27 @@ const SearchBar = ({ onSearch }) => {
     <Paper
       component="form"
       onSubmit={handleSearch}
-      sx={{
-        p: "1px 4px",
-        display: "flex",
-        alignItems: "center",
-        border: "1px solid #ccc",
-        borderRadius: "5px",
-        mr: 1,
-      }}
       className={styles.searchBar}
     >
       <FormControl variant="standard" sx={{ m: 1, minWidth: 110 }}>
         <Select
           value={searchType}
           onChange={(e) => setSearchType(e.target.value)}
-          displayEmpty
           inputProps={{ "aria-label": "Without label" }}
         >
           <MenuItem value="name">Name</MenuItem>
           <MenuItem value="description">Description</MenuItem>
         </Select>
       </FormControl>
+
       <InputBase
-        sx={{ ml: 1, flex: 1 }}
+        sx={{ ml: 1 }}
         placeholder={`Search by ${searchType}`}
         inputProps={{ "aria-label": `search by ${searchType}` }}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+
       <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
         <SearchIcon />
       </IconButton>
