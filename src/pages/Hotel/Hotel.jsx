80ccca50c03ecb6710fce55a5b5809c5dfc402b0
Rahaf Mansour <rahafmansour2018@gmail.com>
2024-02-17 19:09:03 +0200

    split the hotel into multiple files and apply the HOC idea

--- src/pages/Hotel/Hotel.jsx
+++ src/pages/Hotel/Hotel.jsx
@@ -1,17 +1,10 @@
 import { useEffect, useContext } from "react";
-import NavBar from "../../components/NavBar";
-import Footer from "../../components/Footer";
-import CircularProgressIndicator from "../../components/CircularProgressIndicator";
 import { useHotelData } from "./hooks/useHotelData";
-import HotelDetails from "./components/HotelDetails";
-import HotelMapLocation from "./components/HotelMapLocation";
-import AvailableRooms from "./components/AvailableRooms";
-import VisualGallery from "./components/VisualGallery";
-import GenericSnackbar from "../../components/GenericSnackbar";
 import useSnackbar from "../../hooks/useSnackbar";
 import { useParams } from "react-router-dom";
 import { SearchContext } from "../../context/searchContext";
-import styles from "./style.module.css";
+import HotelWithLoading from "./HotelWithLoading";
+import NavBar from "../../components/NavBar";
 
 const Hotel = () => {
   const { hotelId } = useParams();
@@ -39,41 +32,16 @@ const Hotel = () => {
   return (
     <>
       <NavBar />
-      {hotelDetails && (
-        <div>
-          {error && (
-            <p className={styles.errorMessage}>
-              Failed to fetch. please try again later.
-            </p>
-          )}
 
-          <div className={styles.container}>
-            <div className={styles.hotelDetailsAndMapContainer}>
-              <HotelDetails
-                hotelDetails={hotelDetails}
-                hotelGuestReviews={hotelGuestReviews}
-              />
-              {hotelDetails && (
-                <HotelMapLocation
-                  latitude={hotelDetails.latitude}
-                  longitude={hotelDetails.longitude}
-                  hotelName={hotelDetails.hotelName}
-                  location={hotelDetails.location}
-                />
-              )}
-            </div>
-
-            <div className={styles.galleryAndRoomsContainer}>
-              <VisualGallery hotelGallery={hotelGallery} />
-              <AvailableRooms hotelAvailableRooms={hotelAvailableRooms} />
-            </div>
-          </div>
-          <Footer />
-        </div>
-      )}
-
-      <CircularProgressIndicator isLoading={isLoading} />
-      <GenericSnackbar {...snackbar} onClose={handleCloseSnackbar} />
+      <HotelWithLoading
+        isLoading={isLoading}
+        hotelDetails={hotelDetails}
+        hotelGuestReviews={hotelGuestReviews}
+        hotelGallery={hotelGallery}
+        hotelAvailableRooms={hotelAvailableRooms}
+        snackbar={snackbar}
+        handleCloseSnackbar={handleCloseSnackbar}
+      />
     </>
   );
 };
